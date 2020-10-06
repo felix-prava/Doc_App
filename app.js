fd340a3c202418b10ce0311c2509570722691a43
felix24mihai <mihai_prava@yahoo.com>
2020-10-07 02:46:35 +0300

    We can add and edit objects from DB

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/docapp', { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+
+//Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+//Check for DB errors
+db.on('error', function(err){
+    console.log(err);
+});
 
 //Init app
 const app = express();
+
+//Bring in Models
+let ArticleModel = require('./models/article');
 
 //Load View Engine
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Body Parser Middleware
+
+app.use(bodyParser.urlencoded({extended: false}))
+//Parse application/json
+app.use(bodyParser.json());
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Home Route
 app.get('/', function(req, res){
-    let articles = [
-        {
-            id:1,
-            title:'Article One',
-            author:'Felix1',
-            body:'This is article one'
-        },
-        {
-            id:2,
-            title:'Article Two',
-            author:'Felix-Mihai',
-            body:'This is article two'
-        },
-        {
-            id:3,
-            title:'Article three',
-            author:'Felix-Mihai24',
-            body:'This is article three'
+    ArticleModel.find({}, function(err, articles){
+        if (err){
+            console.log(err);
+        } else{
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
         }
-    ];
-    res.render('index', {
-        title: 'Articles',
-        articles: articles
+    });
+});
+
+//Get Single Article
+app.get('/article/:id', function(req, res){
+    ArticleModel.findById(req.params.id, function(err, article){
+        res.render('article', {
+            article: article
+        });  
     });
 });
 
@@ -41,6 +63,64 @@ app.get('/articles/add', function(req, res){
     res.render('add_article', {
         title: 'Add Article'
     });
+});
+
+//Add Submit POST Route
+app.post('/articles/add', function(req, res){
+    let article = new ArticleModel();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+        if (err){
+            console.log(err);
+            return;
+        } else{
+            res.redirect('/');
+        }
+    });
+});
+
+//Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+    ArticleModel.findById(req.params.id, function(err, article){
+        res.render('edit_article', {
+            title: 'Edit Article',
+            article: article
+        });  
+    });
+});
+
+//Update Submit POST Route
+app.post('/articles/edit/:id', function(req, res){
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id};
+
+    ArticleModel.updateOne(query, article, function(err){
+        if (err){
+            console.log(err);
+            return;
+        } else{
+            res.redirect('/');
+        }
+    });
+});
+
+//Delete Article
+app.delete('/articles/:id', function(req, res){
+    let query = {_id:req.params.id};
+
+    //
+
+
+
+
+
 });
 
 //Start Server
