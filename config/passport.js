879cce7d39c407e5b346214fc4184ae88ff4ad5a
felix24mihai <mihai_prava@yahoo.com>
2020-10-08 05:46:31 +0300

    Log in and log out added

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const LocalStrategy = require('passport-local').Strategy;
+const UserModel = require('../models/user');
+const config = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+module.exports = function(passport){
+    //Local Strategy
+    passport.use(new LocalStrategy(function(username, password, done){
+        //Match Username
+        let query = {username: username};
+        UserModel.findOne(query, function(err, user){
+            if(err) throw err;
+            if(!user){
+                return done(null, false, {message: 'No user found'});
+            }
+
+            //Match Password
+            bcrypt.compare(password, user.password, function(err, isMatch){
+                if (err) throw err;
+                if (isMatch){
+                    return done(null, user);
+                } else{
+                    return done(null, false, {message: 'Wrong password'});
+                }
+            });
+        });
+    }));
+
+    passport.serializeUser(function(user, done){
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done){
+        UserModel.findById(id, function(err, user){
+            done(err, user);
+        });
+    });
+}
